fcebf40f9bcefe6e8532f5123c6cc28c07b1177b
remojansen <remo.jansen@wolksoftware.com>
2016-05-13 16:44:40 +0100

    added autoProvide

--- test/utils/auto_wire.test.ts
+++ test/utils/auto_wire.test.ts
@@ -12,6 +12,9 @@ describe("autoProvide", () => {
     it("Should be able to auto-wire binding declarations", () => {
 
         let kernel = new Kernel();
+        // Note @inject annotations are required autoProvide 
+        // even when using classes are used as identifiers
+        // See declaration of Warrior for more details
         autoProvide(kernel, entites);
         let warrior = kernel.get(entites.Warrior);
         expect(warrior.fight()).eql("Using Katana...");
