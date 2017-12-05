794a3e838108b130551cc4a988cecf69df5ff408
remojansen <remo.jansen@wolksoftware.com>
2017-12-05 10:45:53 +0000

    Implemented inversify/InversifyJS/issues/674

--- src/decorator/provide.ts
+++ src/decorator/provide.ts
@@ -1,16 +1,34 @@
-import { decorate, injectable } from "inversify";
+import { decorate, injectable, METADATA_KEY } from "inversify";
 import { interfaces } from "inversify";
 
 function provide(container: interfaces.Container) {
-
-  // function is named for testing
-  return function _provide(serviceIdentifier: interfaces.ServiceIdentifier<any>) {
+  return function _provide(
+    serviceIdentifier: interfaces.ServiceIdentifier<any>,
+    force?: boolean
+  ) {
     let bindingWhenOnSyntax = container.bind<any>(serviceIdentifier).to(<any>null);
     return function (target: any) {
-      decorate(injectable(), target);
-      let binding: interfaces.Binding<any> = (<any>bindingWhenOnSyntax)._binding;
-      binding.implementationType = target;
-      return target;
+
+      const isAlreadyDecorated = Reflect.hasOwnMetadata(METADATA_KEY.PARAM_TYPES, target);
+      const redecorateWithInject = force === true;
+
+     if (redecorateWithInject === true && isAlreadyDecorated === false) {
+        decorate(injectable(), target);
+      } else if (redecorateWithInject === true && isAlreadyDecorated === true) {
+        // Do nothing
+      } else {
+        try {
+          decorate(injectable(), target);
+        } catch (e) {
+          throw new Error(`${e.message} ` +
+          "Please use @provide(ID, true) if you are trying to declare multiple bindings!");
+        }
+     }
+
+     let binding: interfaces.Binding<any> = (<any>bindingWhenOnSyntax)._binding;
+     binding.implementationType = target;
+     return target;
+
     };
   };
 }
